19cbc97211a240285e7849d64a35aa9e5ea14c57
Qiqi Yang <kikiyang03@gmail.com>
2023-03-28 23:52:02 -0400

    Markov jump counts between locations

--- Scripts/Markov jump/MarkovJumpMonthDis.R
+++ Scripts/Markov jump/MarkovJumpMonthDis.R
@@ -51,3 +51,30 @@ ggplot(data=mj.color %>% filter(direction=="SN"),mapping=aes(x=month,y=freq,fill
 
 ggsave(filename='MarkovJumpMonthDis_SN.png',width=9)
 
+ggplot(data=mj.color %>% filter(direction=="NS"),mapping=aes(x=month,y=counts,fill=bird_range))+
+  geom_col()+
+  scale_x_discrete(limits = c("Jan", "Feb","Mar","Apr","May","Jun","Jul",
+                              "Aug","Sep","Oct","Nov","Dec"), 
+                   labels=c("Jan", "","Mar","","May",
+                            "","Jul","","Sep","","Nov","")) + 
+  facet_wrap(~od) +
+  scale_fill_manual(values=c('#D99D7A','#EEEA76','#9CBEE2','grey'))+
+  # geom_hline(yintercept=1/6,linetype='dashed',colour='darkred') +
+  ylab('Markov jump counts') +
+  labs(fill="Bird annual cycle\nphase")
+ggsave(filename='MarkovJumpCountsMonthDis_NS.png',width=9)
+
+ggplot(data=mj.color %>% filter(direction=="SN"),mapping=aes(x=month,y=counts,fill=bird_range))+
+  geom_col()+
+  scale_x_discrete(limits = c("Jan", "Feb","Mar","Apr","May","Jun","Jul",
+                              "Aug","Sep","Oct","Nov","Dec"), 
+                   labels=c("Jan", "","Mar","","May",
+                            "","Jul","","Sep","","Nov","")) + 
+  facet_wrap(~od) +
+  scale_fill_manual(values=c('#D99D7A','#EEEA76','#9CBEE2','grey'))+
+  # geom_hline(yintercept=1/6,linetype='dashed',colour='darkred') +
+  ylab('Markov jump counts') +
+  labs(fill="Bird annual cycle\nphase")
+
+ggsave(filename='MarkovJumpCountsMonthDis_SN.png',width=9)
+
